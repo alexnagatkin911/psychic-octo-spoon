954198666bbf267df102c5ea85b669d18eb4d80f
alexnagatkin911@gmail.com <alexnagatkin911@gmail.com>
2017-02-28 04:18:08 +0000

    Added User Model with validation Added Micropost Model with validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170227001642) do
+ActiveRecord::Schema.define(version: 20170228040714) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
